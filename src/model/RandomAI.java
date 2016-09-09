f594134682874a00346f8125765d3fd798398738
haoqing yan <haoqingyan@dhcp-10-132-164-155.uawifi.arizona.edu>
2016-09-09 10:49:29 -0700

    Completed RandomAI

--- src/model/RandomAI.java
+++ src/model/RandomAI.java
@@ -1,3 +1,4 @@
+//Haoqing Yan
 package model;
 
 import java.awt.Point;
@@ -13,5 +14,26 @@ import java.util.Random;
 
 // There is an intentional compile time error.  Implement this interface
 public class RandomAI implements TicTacToeStrategy {
+	private static Random generator;
+	public RandomAI() {
+	    generator = new Random();
+	  } 
+	@Override
+	public Point desiredMove(TicTacToeGame theGame) {
+		boolean set = false;
+	    while (!set) {
+	      if (theGame.maxMovesRemaining() == 0)
+	        throw new IGotNowhereToGoException(
+	            " -- Hey there programmer, the board is filled");
 
+	      // Otherwise, try to randomly find an open spot 
+	      int row = generator.nextInt(3);
+	      int col = generator.nextInt(theGame.size());
+	      if (theGame.available(row, col)) {
+	        set = true;
+	        return new Point(row, col);
+	      }
+	    }
+	    return null;
+	}
 }
